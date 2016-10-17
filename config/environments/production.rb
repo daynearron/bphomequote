8363c2e2887243176c9c794e1650e1f612ed5ba5
Richard Young <richard.j.young@gmail.com>
2016-10-17 12:55:17 -0500

    update env var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :user_name => ENV["SENDGRID_USERNAME"],
+    :user_name => ENV["SENDGRID_API_KEY"],
     :password => ENV["SENDGRID_PASSWORD"],
     :address => 'smtp.sendgrid.net',
     :port => 587,
